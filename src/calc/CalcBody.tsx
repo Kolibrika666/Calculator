ff488a48010f4427337bd5375620f15eb84f253d
Kolibrika666 <kolibrika666@gmail.com>
2023-12-14 22:11:39 +0700

    init: invalid ButtonNumber

--- src/calc/CalcBody.tsx
+++ src/calc/CalcBody.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import s from "./Calc.module.scss";
-import Button from "./button/Button";
 import CalcScreen from "./screen/screen";
+import ButtonNumber from "./button/NumberButtom";
+import ExpressionScreen from "./ExpressionScreen";
+import c from "./screen/screen.module.scss";
 
 function CalcBody() {
+  const heandler = () => {};
   return (
     <main>
-      <CalcScreen />
+      <div className={c.screen__container}>
+        <div className={s.expression}>1234 - 24</div>;
+        <div id="rezult" className={s.rezult}></div>
+      </div>
       <div className={s.white__container}></div>
-      <Button />
+      <ButtonNumber onClick={heandler} />
     </main>
   );
 }
--- src/calc/screen/rezultScreen.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import s from "./screen.module.scss";
-
-const Rezult = () => {
-  return <div className={s.rezult}>1123</div>;
-};
-
-export default Rezult;
